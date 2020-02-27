2b626b08e1199f66ce8e5d01baabfc24d2fb65cd
thedcm <dimon-khr@mail.ru>
2020-02-27 21:42:53 +0300

    Добавьте файлы проекта.

---
+++ TestTaskApdax/Controllers/ValidationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestTaskApdax.Controllers
+{
+    [Route("[controller]")]
+    public class ValidationController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<string> Post()
+        {
+            string body;
+
+            using (var streamReader = new StreamReader(Request.Body))
+            {
+                body = await streamReader.ReadToEndAsync();
+            }
+
+            try
+            {
+                JToken t1 = JToken.Parse(body);
+            }
+            catch
+            {
+                return "{\"error\": \"Invalid JSON\"}";
+            }
+
+            int levels = 0;
+
+            JsonTextReader reader = new JsonTextReader(new StringReader(body));
+            while (reader.Read())
+                if (levels < reader.Depth)
+                    levels = reader.Depth;
+
+            return "{\"levels\": " + levels + "}";
+        }
+    }
+}
